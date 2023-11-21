7f46bfd4a6f9da7dea74813a96c9e08693e0fa18
mohammedchakir <zerroukimedchakir@gmail.com>
2023-11-21 08:54:34 -0800

    add new files of rest of tasks

---
+++ deck.h
@@ -0,0 +1,52 @@
+#ifndef DECK_H
+#define DECK_H
+
+#include <stddef.h>
+
+/**
+ * enum kind_e - Kinds of playing cards
+ *
+ * @SPADE: Spades
+ * @HEART: Hearts
+ * @CLUB: Clubs
+ * @DIAMOND: Diamonds
+ */
+typedef enum kind_e
+{
+	SPADE = 0,
+	HEART,
+	CLUB,
+	DIAMOND
+} kind_t;
+
+/**
+ * struct card_s - Playing card
+ *
+ * @value: Value of the card (Ace to King)
+ * @kind: Kind of the card
+ */
+typedef struct card_s
+{
+	const char *value;
+	const kind_t kind;
+} card_t;
+
+/**
+ * struct deck_node_s - Node of a deck of cards
+ *
+ * @card: Pointer to the card of the node
+ * @prev: Pointer to the previous node in the deck
+ * @next: Pointer to the next node in the deck
+ */
+typedef struct deck_node_s
+{
+	const card_t *card;
+	struct deck_node_s *prev;
+	struct deck_node_s *next;
+} deck_node_t;
+
+/* Function prototypes */
+void print_deck(const deck_node_t *deck);
+void sort_deck(deck_node_t **deck);
+
+#endif /* DECK_H */
